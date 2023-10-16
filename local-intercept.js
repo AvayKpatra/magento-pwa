d0cbc41a4125f4eb748a6098a432b9a61fa0a57a
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2023-10-16 19:23:54 +0530

    initial commit

---
+++ local-intercept.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+/**
+ * Custom interceptors for the project.
+ *
+ * This project has a section in its package.json:
+ *    "pwa-studio": {
+ *        "targets": {
+ *            "intercept": "./local-intercept.js"
+ *        }
+ *    }
+ pwa-studio": {
+   "targets": {
+   "intercept": "src/targets/local-intercept"
+   }
+ }
+ *
+ * This instructs Buildpack to invoke this file during the intercept phase,
+ * as the very last intercept to run.
+ *
+ * A project can intercept targets from any of its dependencies. In a project
+ * with many customizations, this function would tap those targets and add
+ * or modify functionality from its dependencies.
+ */
+
+//  pwa-studio": {
+//     "targets": {
+//     "intercept": "src/targets/local-intercept"
+//     }
+//   }
+
+
+
+function localIntercept() {}
+
+
+module.exports = localIntercept;
---
+++ src/targets/local-intercept.js
@@ -0,0 +1,11 @@
+module.exports = (targets) => {
+    targets.of("@magento/venia-ui").routes.tap((routes) => {
+      routes.push({
+        name: "MFooter",
+        pattern: "/footer/:who?",
+        path: require.resolve("../components/Footer/footer.js"),
+      });
+      return routes;
+    });
+  };
+  
