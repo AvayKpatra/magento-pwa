02305bb20272f7b1ac182dbef66a0e897e743938
AvayKpatra <147364657+AvayKpatra@users.noreply.github.com>
2023-10-18 18:58:45 +0530

    Adding products detail using graphql

--- componentOverrideMapping.js
+++ componentOverrideMapping.js
@@ -2,5 +2,8 @@ const veniaUiPackagePath = '@magento/venia-ui';
 module.exports = componentOverrideMapping = {
     [`${veniaUiPackagePath}/lib/components/Footer/footer.js`]: 'src/components/Footer/footer.js', 
     [`${veniaUiPackagePath}/lib/components/MegaMenu/megaMenuItem.js`]: 'src/components/MegaMenu/megaMenuItem.js', 
+    [`${veniaUiPackagePath}/lib/components/ProductFullDetail/productFullDetail.js`]: 'src/components/ProductFullDetail/productFullDetail.js',
+    [`${veniaUiPackagePath}/lib/components/Gallery/item.js`]: 'src/components/Gallery/item.js',
+    
     // add comma-separated files
 };
